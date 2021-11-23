05f5c203765e4d9c68340a6f01b30c0bcf049e0d
John Barbour <jbarbour3@gatech.edu>
2021-11-22 22:06:40 -0500

    updated to google example

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 
+import os
 
-def print_something(string):
-    print(string)
-    return string
+from flask import Flask
 
-if __name__ == '__main__':
-    print_something("something")
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    name = os.environ.get("NAME", "World")
+    return "Hello {}!".format(name)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
